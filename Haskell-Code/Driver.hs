e6a97fa367728c094353da1cc46b5b2c760fd0ed
lukababu <luke.iremadze@ucalgary.ca>
2018-03-10 15:47:45 -0700

    Beginning of assignment

--- Haskell-Code/Driver.hs
+++ Haskell-Code/Driver.hs
@@ -2,5 +2,19 @@
     Driver File
     
 	-}
+import System.IO
+import Control.Monad
 
-main = putStrLn "Hello, World!"
+data Machine = One | Two | Three | Four | Five | Six | Seven | Eight deriving (Read, Show, Enum, Eq, Ord)
+data Task = A | B | C | D | E | F | G | H deriving (Read, Show, Enum, Eq, Ord)
+
+type Name = [Char]
+data Assisngment = Partial Machine Task | Forbidden Machine Task | NearTask Task Task | NearPenalities Task Task Machine deriving (Show)
+type Penalities = [[Int]]
+
+main = do
+    handle <- openFile "test.txt" ReadMode
+    contents <- hGetLine handle
+    putStr contents
+
+    hClose handle
