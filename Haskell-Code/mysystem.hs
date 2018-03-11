ff530ae54957b486887e1429cb70712bfda1eea1
sim-97 <sim.bhattarai@gmail.com>
2018-03-11 17:13:15 -0600

    Created mysystem.hs

---
+++ Haskell-Code/mysystem.hs
@@ -0,0 +1,30 @@
+import System.Environment
+import System.IO  
+import Control.Monad  
+import System.Directory  
+import Data.List  
+
+--The main method will take in command line arguments and read file into a string. 
+-- to execute ./mysystem arg1 arg2
+
+main = do
+    args <- getArgs
+    putStrLn "The filemanes are:"
+    mapM putStrLn args  
+    putStrLn "The head name is:"
+    putStrLn (outfile args)
+    view (infile args)
+    view (outfile args)
+
+--Prints the given filename to console
+view :: [String] -> IO ()  
+view [fileName] = do  
+    contents <- readFile fileName  
+    let todoTasks = lines contents  
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks  
+    putStr $ unlines numberedTasks
+
+--Strips the array of argument to to find the input file
+infile :: [String] -> [String]
+infile [] = error "No input fileName given!"
+infile (x:_) = [x]
