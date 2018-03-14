7b4d300948018edf6c7ea2b9fdfb23347926aa01
sim-97 <sim.bhattarai@gmail.com>
2018-03-14 16:23:14 -0600

    Update
    Latest version check for errors in file and if there is an error, it correctly returns a file that says "Error while Parsing"

--- Haskell-Code/mysystem.hs
+++ Haskell-Code/mysystem.hs
@@ -3,32 +3,44 @@ import System.IO
 import Control.Monad  
 import System.Directory  
 import Data.List  
+import Data.String
+import System.Exit
+import ParseLib
 
 --The main method will take in command line arguments and read file into a string. 
--- to execute ./mysystem arg1 arg2
+-- to execute ./filename arg1 arg2
 
 main = do
     args <- getArgs
-    putStrLn "The filemanes are:"
-    mapM putStrLn args  
-    putStrLn "The head name is:"
-    putStrLn (outfile args)
-    view (infile args)
-    --view (tail args) This code was for test purposes only. Test: Passed
-    --Stores BBBCCC in aaa
-    --writes BBBCCC into the file name that is passed as arg2
-    let aaa  = "BBB" ++ "CCC"
-    writeFile (last args) aaa --Works correctly
+    handle <- openFile (args!! 0) ReadMode
+    contents <- (hGetContents handle)
+    --filecontent not contails all of the contents in the file. 
+    let filecontent = map noEndSpaces (lines (noCR contents))
+    --putStr $ unlines filecontent
+        --Now we split it into sections
+        --First sections that is sliced is name
+        nameSec = sliced "" "forced partial assignment:" filecontent
+        --Forced partial Assignment
+        forcedSec = sliced "forced partial assignment:" "forbidden machine:" filecontent
+        --Fobidden machines
+        forbidSec = sliced "forbidden machine:" "too-near tasks:" filecontent
+        --Too near Task
+        tntSec = sliced "too-near tasks:" "machine penalties:" filecontent
+        --Machine pentalties
+        machPenSec = sliced "machine penalties:" "too-near penalities" filecontent
+        --too near penalties
+        tnpSec = sliced "too-near penalities" "" filecontent
+    --Now that we have each part of the code sliced from section to section
+    --We have to check if the spellings are correct for all the sections
+    --If the spellings are incorrectl, the string tokens passed as arguments earlier won't be found 
+    --this will result in a creation of an empty string
+    --Here we will check if the strings are empty or not
+    --this returns "Error while parsing file" if the file does not have the correct input
+    if (elem 0 (map length [nameSec, forcedSec, forcedSec, tnpSec, machPenSec, tnpSec]))
+    then do writeFile (last args) "Error while parsing file"
+            exitSuccess
+    else return ()
+    --let aaa  = "BBB" ++ "CCC"
+    --writeFile (last args) (unlines nameSec)
+    
 
---Prints the given filename to console
-view :: [String] -> IO ()  
-view [fileName] = do  
-    contents <- readFile fileName  
-    let todoTasks = lines contents  
-        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks  
-    putStr $ unlines numberedTasks
-
---Strips the array of argument to to find the input file
-infile :: [String] -> [String]
-infile [] = error "No input fileName given!"
-infile (x:_) = [x]
