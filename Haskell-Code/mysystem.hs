fbc9c999ec9f7f2aff67bb267e81d8763f13223a
sim-97 <sim.bhattarai@gmail.com>
2018-03-14 18:10:50 -0600

    Update mysystem.hs

--- Haskell-Code/mysystem.hs
+++ Haskell-Code/mysystem.hs
@@ -19,7 +19,7 @@ main = do
     --putStr $ unlines filecontent
         --Now we split it into sections
         --First sections that is sliced is name
-        nameSec = sliced "" "forced partial assignment:" filecontent
+        nameSec = sliced "Name:" "forced partial assignment:" filecontent
         --Forced partial Assignment
         forcedSec = sliced "forced partial assignment:" "forbidden machine:" filecontent
         --Fobidden machines
@@ -32,15 +32,20 @@ main = do
         tnpSec = sliced "too-near penalities" "" filecontent
     --Now that we have each part of the code sliced from section to section
     --We have to check if the spellings are correct for all the sections
-    --If the spellings are incorrectl, the string tokens passed as arguments earlier won't be found 
+    --If the spellings are incorrectly, the string tokens passed as arguments earlier won't be found 
     --this will result in a creation of an empty string
     --Here we will check if the strings are empty or not
     --this returns "Error while parsing file" if the file does not have the correct input
     if (elem 0 (map length [nameSec, forcedSec, forcedSec, tnpSec, machPenSec, tnpSec]))
-    then do writeFile (last args) "Error while parsing file"
+    then do writeFile (last args) "Error while parsing input file"
             exitSuccess
     else return ()
+    --We have now checked for errors in spellings
+    --Now we want to check for errors with inputs
+    --First, lets check if there is error under name.
+    if ((length(delWhitespaceLines nameSec)/=1))
+    then do writeFile (last args) "Error while parsing input file"
+            exitSuccess
+    else return()
     --let aaa  = "BBB" ++ "CCC"
     --writeFile (last args) (unlines nameSec)
-    
-
