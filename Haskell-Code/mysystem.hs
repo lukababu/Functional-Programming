defc6ca14a2e7b5b62a6a1d76babc71f61163102
lukababu <luke.iremadze@ucalgary.ca>
2018-03-18 00:31:38 -0600

    Output updated

--- Haskell-Code/mysystem.hs
+++ Haskell-Code/mysystem.hs
@@ -17,20 +17,13 @@ main = do
     contents <- (hGetContents handle)
     --filecontent not contails all of the contents in the file. 
     let filecontent = map noEndSpaces (lines (noCR contents))
-    --putStr $ unlines filecontent
-        --Now we split it into sections
-        --First sections that is sliced is name
         nameSec = sliced "Name:" "forced partial assignment:" filecontent
-        --Forced partial Assignment
         forcedSec = sliced "forced partial assignment:" "forbidden machine:" filecontent
-        --Fobidden machines
         forbidSec = sliced "forbidden machine:" "too-near tasks:" filecontent
-        --Too near Task
         tntSec = sliced "too-near tasks:" "machine penalties:" filecontent
-        --Machine pentalties
         machPenSec = sliced "machine penalties:" "too-near penalities" filecontent
-        --too near penalties
         tnpSec = sliced "too-near penalities" "" filecontent
+    
     --Now that we have each part of the code sliced from section to section
     --We have to check if the spellings are correct for all the sections
     --If the spellings are incorrectly, the string tokens passed as arguments earlier won't be found 
@@ -41,6 +34,7 @@ main = do
     then do writeFile (last args) "Error while parsing input file"
             exitSuccess
     else return ()
+    
     --We have now checked for errors in spellings
     --Now we want to check for errors with inputs
     --First, lets check if there is error under name.
@@ -48,20 +42,21 @@ main = do
     then do writeFile (last args) "Error while parsing input file"
             exitSuccess
     else return()
+    
     --After name, we want to formats of the rest.
     --First we need to ensure the input is a 2-tuple.
     if(anding (map twotup(delWhitespaceLines forcedSec)))
     then return()
-    else do writeFile (last args) "invalid machine/task - not a 2-tuple forced"
+    else do writeFile (last args) "invalid machine/task"
             exitSuccess
  
     --Now we can check that forced assignments is actually a valid machine and task pair
     if (anding(map validMTPair(delWhitespaceLines forcedSec)))
     then return()
-    else do writeFile (last args) "Invalid Machine/Task pair - 1"
+    else do writeFile (last args) "invalid machine/task"
             exitSuccess
             
-     -- look for duplicates
+    -- look for duplicates
     let forced = parseTT(delWhitespaceLines forcedSec)
     if ((duplicates(map fst forced)) || (duplicates(map snd forced)))
     then do writeFile (last args) "partial assignment error"
@@ -71,36 +66,36 @@ main = do
     --Ensure forbidden machines are a 2-tuple.
     if(anding (map twotup(delWhitespaceLines forbidSec)))
     then return()
-    else do writeFile (last args) "invalid machine/task - not a 2-tuple forbid"
+    else do writeFile (last args) "invalid machine/task"
             exitSuccess
 
     --Ensure too near task is a 2-tuple
     if(anding (map twotup(delWhitespaceLines tntSec)))
     then return()
-    else do writeFile (last args) "invalid machine/task - not a 2-tuple too near task"
+    else do writeFile (last args) "invalid machine/task"
             exitSuccess
 
     --Ensures too near penalty is a 3-tuple
     if(anding (map tuple3(delWhitespaceLines tnpSec)))
     then return()
-    else do writeFile (last args) "invalid machine/task - not a 3-tuple too near p"
+    else do writeFile (last args) "invalid machine/task"
             exitSuccess
 
     --Now we can check that forbidden assignments is actually a valid machine and task pair
     if (anding(map validMTPair(delWhitespaceLines forbidSec)))
     then return()
-    else do writeFile (last args) "Invalid Machine/Task pair - 2"
+    else do writeFile (last args) "invalid machine/task"
             exitSuccess
 
     --Now we check if the too near task is valid
     if (anding(map validTNTPair(delWhitespaceLines tntSec)))
     then return()
-    else do writeFile (last args) "Invalid Task/Task pair"
+    else do writeFile (last args) "invalid task"
             exitSuccess
 
     --Ensure that the machine penalty is vertically of height 8
     if(length(delWhitespaceLines machPenSec) /= 8)
-    then do writeFile (last args) "Machine Penalty Error"
+    then do writeFile (last args) "machine penalty error"
             exitSuccess 
     else return()
     
@@ -108,16 +103,24 @@ main = do
     --print machPen
     --ensures that the penalty matrix has total elements of 64
     if(sum(map length machPen)/= 64)
-    then do writeFile (last args) "Machine Penalty Error"
+    then do writeFile (last args) "machine penalty error"
             exitSuccess 
     else return()
     
     --Now we can check that forbidden assignments is actually a valid machine and task pair
     if (anding(map validTTtrip(delWhitespaceLines tnpSec)))
     then return()
-    else do writeFile (last args) "Invalid task"
+    else do writeFile (last args) "invalid task"
             exitSuccess
+
+    --putStr forcedSec
+    writeFile (last args) (processIO (parseMT forcedSec) (parseMT forbidSec) (parseTT tntSec) machPenSec (parseTTx tnpSec))
     
 
-    --let aaa  = "BBB" ++ "CCC"
-    --writeFile (last args) (unlines nameSec)
+-- FPA, FM, TNT, Pen, TNP
+processIO :: [(Int, Int)] -> [(Int, Int)] -> [(Int, Int)] -> [a] -> [(Int,Int,Int)] -> String
+--processIO fpa _ _ _ _ = "Yup"
+processIO fpa fma tnt pen tnp = show (getAssignment fpa fma tnt)
+
+--processFPA :: [a] -> [(Int, Int)]
+--processFPA
